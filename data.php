78a7411feb7c866fee6e845d321efd1dab4645f3
Trish <38753765+Trish-Mundus@users.noreply.github.com>
2018-08-01 18:37:23 +0500

    Create data.php

---
+++ data.php
@@ -0,0 +1,356 @@
+<?php
+$data = array(
+	0 => array(
+		'name' => 'Бразилия', # название команды
+		'games' => 104, # всего игр
+		'win' => 70, # количество побед
+		'draw' => 17, # количестве ничей
+		'defeat' => 17, # количество поражений
+		'goals' => array(
+			'scored' => 221, # всего забито голов
+			'skiped' => 102, # всего пропущено голов
+		),
+	),
+	1 => array(
+		'name' => 'Германия / ФРГ',
+		'games' => 106,
+		'win' => 66,
+		'draw' => 20,
+		'defeat' => 20,
+		'goals' => array(
+			'scored' => 224,
+			'skiped' => 121,
+		),
+	),
+	2 => array(
+		'name' => 'Италия',
+		'games' => 83,
+		'win' => 45,
+		'draw' => 21,
+		'defeat' => 17,
+		'goals' => array(
+			'scored' => 128,
+			'skiped' => 77,
+		),
+	),
+	3 => array(
+		'name' => 'Аргентина',
+		'games' => 77,
+		'win' => 42,
+		'draw' => 14,
+		'defeat' => 21,
+		'goals' => array(
+			'scored' => 131,
+			'skiped' => 84,
+		),
+	),
+	4 => array(
+		'name' => 'Англия',
+		'games' => 62,
+		'win' => 26,
+		'draw' => 20,
+		'defeat' => 16,
+		'goals' => array(
+			'scored' => 79,
+			'skiped' => 56,
+		),
+	),
+	5 => array(
+		'name' => 'Испания',
+		'games' => 59,
+		'win' => 29,
+		'draw' => 12,
+		'defeat' => 18,
+		'goals' => array(
+			'scored' => 92,
+			'skiped' => 66,
+		),
+	),
+	6 => array(
+		'name' => 'Франция',
+		'games' => 59,
+		'win' => 28,
+		'draw' => 12,
+		'defeat' => 19,
+		'goals' => array(
+			'scored' => 106,
+			'skiped' => 71,
+		),
+	),
+	7 => array(
+		'name' => 'Голландия',
+		'games' => 50,
+		'win' => 27,
+		'draw' => 12,
+		'defeat' => 11,
+		'goals' => array(
+			'scored' => 86,
+			'skiped' => 48,
+		),
+	),
+	8 => array(
+		'name' => 'Уругвай',
+		'games' => 51,
+		'win' => 20,
+		'draw' => 12,
+		'defeat' => 19,
+		'goals' => array(
+			'scored' => 80,
+			'skiped' => 71,
+		),
+	),
+	9 => array(
+		'name' => 'Швеция',
+		'games' => 46,
+		'win' => 16,
+		'draw' => 13,
+		'defeat' => 17,
+		'goals' => array(
+			'scored' => 74,
+			'skiped' => 69,
+		),
+	),
+	10 => array(
+		'name' => 'Россия / СССР',
+		'games' => 40,
+		'win' => 17,
+		'draw' => 8,
+		'defeat' => 15,
+		'goals' => array(
+			'scored' => 66,
+			'skiped' => 47,
+		),
+	),
+	11 => array(
+		'name' => 'Сербия / Югославия    ',
+		'games' => 43,
+		'win' => 17,
+		'draw' => 8,
+		'defeat' => 18,
+		'goals' => array(
+			'scored' => 64,
+			'skiped' => 59,
+		),
+	),
+	12 => array(
+		'name' => 'Мексика',
+		'games' => 53,
+		'win' => 14,
+		'draw' => 14,
+		'defeat' => 25,
+		'goals' => array(
+			'scored' => 57,
+			'skiped' => 92,
+		),
+	),
+	13 => array(
+		'name' => 'Бельгия',
+		'games' => 41,
+		'win' => 14,
+		'draw' => 9,
+		'defeat' => 18,
+		'goals' => array(
+			'scored' => 52,
+			'skiped' => 66,
+		),
+	),
+	14 => array(
+		'name' => 'Польша',
+		'games' => 31,
+		'win' => 15,
+		'draw' => 5,
+		'defeat' => 11,
+		'goals' => array(
+			'scored' => 44,
+			'skiped' => 40,
+		),
+	),
+	15 => array(
+		'name' => 'Венгрия',
+		'games' => 32,
+		'win' => 15,
+		'draw' => 3,
+		'defeat' => 14,
+		'goals' => array(
+			'scored' => 87,
+			'skiped' => 57,
+		),
+	),
+	16 => array(
+		'name' => 'Португалия',
+		'games' => 26,
+		'win' => 13,
+		'draw' => 4,
+		'defeat' => 9,
+		'goals' => array(
+			'scored' => 43,
+			'skiped' => 29,
+		),
+	),
+	17 => array(
+		'name' => 'Чехия / Чехословакия',
+		'games' => 33,
+		'win' => 12,
+		'draw' => 5,
+		'defeat' => 16,
+		'goals' => array(
+			'scored' => 47,
+			'skiped' => 49,
+		),
+	),
+	18 => array(
+		'name' => 'Чили',
+		'games' => 33,
+		'win' => 11,
+		'draw' => 7,
+		'defeat' => 15,
+		'goals' => array(
+			'scored' => 40,
+			'skiped' => 49,
+		),
+	),
+	19 => array(
+		'name' => 'Австрия',
+		'games' => 29,
+		'win' => 12,
+		'draw' => 4,
+		'defeat' => 13,
+		'goals' => array(
+			'scored' => 43,
+			'skiped' => 47,
+		),
+	),
+	20 => array(
+		'name' => 'Швейцария',
+		'games' => 33,
+		'win' => 11,
+		'draw' => 6,
+		'defeat' => 16,
+		'goals' => array(
+			'scored' => 45,
+			'skiped' => 59,
+		),
+	),
+	21 => array(
+		'name' => 'Парагвай',
+		'games' => 27,
+		'win' => 7,
+		'draw' => 10,
+		'defeat' => 10,
+		'goals' => array(
+			'scored' => 30,
+			'skiped' => 38,
+		),
+	),
+	22 => array(
+		'name' => 'США',
+		'games' => 33,
+		'win' => 8,
+		'draw' => 6,
+		'defeat' => 19,
+		'goals' => array(
+			'scored' => 37,
+			'skiped' => 62,
+		),
+	),
+	23 => array(
+		'name' => 'Румыния',
+		'games' => 21,
+		'win' => 8,
+		'draw' => 5,
+		'defeat' => 8,
+		'goals' => array(
+			'scored' => 30,
+			'skiped' => 32,
+		),
+	),
+	24 => array(
+		'name' => 'Южная Корея',
+		'games' => 31,
+		'win' => 5,
+		'draw' => 9,
+		'defeat' => 17,
+		'goals' => array(
+			'scored' => 31,
+			'skiped' => 67,
+		),
+	),
+	25 => array(
+		'name' => 'Дания',
+		'games' => 16,
+		'win' => 8,
+		'draw' => 2,
+		'defeat' => 6,
+		'goals' => array(
+			'scored' => 27,
+			'skiped' => 24,
+		),
+	),
+	26 => array(
+		'name' => 'Хорватия',
+		'games' => 16,
+		'win' => 7,
+		'draw' => 2,
+		'defeat' => 7,
+		'goals' => array(
+			'scored' => 21,
+			'skiped' => 17,
+		),
+	),
+	27 => array(
+		'name' => 'Колумбия',
+		'games' => 18,
+		'win' => 7,
+		'draw' => 2,
+		'defeat' => 9,
+		'goals' => array(
+			'scored' => 26,
+			'skiped' => 27,
+		),
+	),
+	28 => array(
+		'name' => 'Шотландия',
+		'games' => 23,
+		'win' => 4,
+		'draw' => 7,
+		'defeat' => 12,
+		'goals' => array(
+			'scored' => 25,
+			'skiped' => 41,
+		),
+	),
+	29 => array(
+		'name' => 'Камерун',
+		'games' => 23,
+		'win' => 4,
+		'draw' => 7,
+		'defeat' => 12,
+		'goals' => array(
+			'scored' => 18,
+			'skiped' => 43,
+		),
+	),
+	30 => array(
+		'name' => 'Коста-Рика',
+		'games' => 15,
+		'win' => 5,
+		'draw' => 4,
+		'defeat' => 6,
+		'goals' => array(
+			'scored' => 17,
+			'skiped' => 23,
+		),
+	),
+	31 => array(
+		'name' => 'Болгария',
+		'games' => 26,
+		'win' => 3,
+		'draw' => 8,
+		'defeat' => 15,
+		'goals' => array(
+			'scored' => 22,
+			'skiped' => 53,
+		),
+	),
+);
+?>
